00bc200df9036d06ff1dae2983c866d9e54e0945
sherruh <sherruh@gmail.com>
2020-01-30 22:11:30 +0600

    Initial commit

---
+++ gradlew.bat
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<resources xmlns:ns1="urn:oasis:names:tc:xliff:document:1.2">
+    <string msgid="7903180715631665059" name="abc_action_bar_home_description">"转到首页"</string>
+    <string msgid="6794660482873516081" name="abc_action_bar_up_description">"转到上一层级"</string>
+    <string msgid="1155814932213556626" name="abc_action_menu_overflow_description">"更多选项"</string>
+    <string msgid="2571498368472823490" name="abc_action_mode_done">"完成"</string>
+    <string msgid="3732416590524162402" name="abc_activity_chooser_view_see_all">"查看全部"</string>
+    <string msgid="1798588241954930982" name="abc_activitychooserview_choose_application">"选择应用"</string>
+    <string msgid="3403923230105792483" name="abc_capital_off">"关闭"</string>
+    <string msgid="7831734969929204599" name="abc_capital_on">"开启"</string>
+    <string msgid="5725160506500770567" name="abc_menu_alt_shortcut_label">"Alt+"</string>
+    <string msgid="944415252197684443" name="abc_menu_ctrl_shortcut_label">"Ctrl+"</string>
+    <string msgid="129742188101734366" name="abc_menu_delete_shortcut_label">"Delete 键"</string>
+    <string msgid="6840127756824236027" name="abc_menu_enter_shortcut_label">"Enter 键"</string>
+    <string msgid="4974283687810130415" name="abc_menu_function_shortcut_label">"Fn+"</string>
+    <string msgid="8046416353848716905" name="abc_menu_meta_shortcut_label">"Meta+"</string>
+    <string msgid="3271697756921353410" name="abc_menu_shift_shortcut_label">"Shift+"</string>
+    <string msgid="2304645930658438191" name="abc_menu_space_shortcut_label">"空格键"</string>
+    <string msgid="8327365089695024394" name="abc_menu_sym_shortcut_label">"Sym+"</string>
+    <string msgid="3570106412128999382" name="abc_prepend_shortcut_label">"Menu+"</string>
+    <string msgid="940844115270746197" name="abc_search_hint">"搜索…"</string>
+    <string msgid="1769270744562318534" name="abc_searchview_description_clear">"清除查询"</string>
+    <string msgid="908784302972860853" name="abc_searchview_description_query">"搜索查询"</string>
+    <string msgid="5466662225065974044" name="abc_searchview_description_search">"搜索"</string>
+    <string msgid="8203855622131699655" nam
