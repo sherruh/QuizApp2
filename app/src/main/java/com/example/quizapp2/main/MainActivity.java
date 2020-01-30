fa9a39a0bf3b74eed8bebc82ce0708a351474096
sherruh <sherruh@gmail.com>
2020-01-30 23:58:46 +0600

    Add ViewPager

--- app/src/main/java/com/example/quizapp2/main/MainActivity.java
+++ app/src/main/java/com/example/quizapp2/main/MainActivity.java
@@ -1,21 +1,62 @@
 package com.example.quizapp2.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
 
 import com.example.quizapp2.R;
+import com.example.quizapp2.settings.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.main_frame,new MainFragment())
-                .commit();
+        initView();
+    }
+
+    private void initView() {
+        viewPager = findViewById(R.id.main_view_pager);
+        pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    public class PagerAdapter extends FragmentPagerAdapter{
+
+        public PagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+
+            Fragment fragment;
+            switch (position){
+                case 1:
+                    fragment = MainFragment.newInstance();
+                    break;
+                default:
+                    fragment = SettingsFragment.newInstance();
+            }
+
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
     }
 }
